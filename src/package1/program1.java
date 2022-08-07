572a33c9854c7c5c92388031432c893ea2114c3c
Aks <Aks@DESKTOP-INPJBSU>
2022-08-07 18:56:00 +0530

    Akshay - send a new code

--- src/package1/program1.java
+++ src/package1/program1.java
@@ -9,6 +9,10 @@ public class program1
 		
 		System.out.println("Hiii i am akshay Ghule");
 		
+		System.out.println("Never Give up dude");
+		
+		System.out.println("No one can do for me only i can change my life");
+		
 	}
 
 }
